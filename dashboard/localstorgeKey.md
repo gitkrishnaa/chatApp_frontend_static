7fdd587da03ddc8733b52a7446676f3c3a095079
krishna kumar <kri821084@gmail.com>
2023-06-12 22:37:04 +0530

    logout function and button working fine dashoard load working

---
+++ dashboard/localstorgeKey.md
@@ -0,0 +1,6 @@
+from login component
+localStorage.setItem("jwtKey",jwtKey);
+//savin user bame
+localStorage.setItem("user_name",respObject.user_name);
+localStorage.setItem("user_id",respObject.user_id);
+localStorage.setItem("user_email",respObject.user_email);
---
+++ dashboard/main.js
@@ -0,0 +1,26 @@
+
+const jwt=localStorage.getItem("jwtKey")
+const user_name=localStorage.getItem("user_name")
+const user_id=localStorage.getItem("user_id")
+const user_email=localStorage.getItem("user_email")
+
+//checking is user logout or bot
+if(!jwt){
+  const respose=  confirm("user logout since jwtKey not in loaclstorage ,would you like to login or signup")
+  if(respose){window.location="../index.html"}
+  return;
+}
+else{
+    alert(`welcome to chatApp ${user_name}`)
+}
+
+const logout_button=document.getElementById("logout_button");
+logout_button.addEventListener("click",()=>{
+    
+localStorage.removeItem("jwtKey")
+localStorage.removeItem("user_name")
+localStorage.removeItem("user_id")
+localStorage.removeItem("user_email") 
+window.location="../index.html"
+
+})
